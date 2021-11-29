96c5829345cb6b1ad69faa9c95d63c18ce6bb729
Tsygankovae.a <Tsygankovae.a@outlook.com>
2021-11-29 22:52:24 +0300

    Добавьте файлы проекта.

---
+++ Задание 6.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку без знаков препинания");
+            string startString  = Console.ReadLine();
+            string [] startArray = startString.Split();
+            int n = 0;
+            int m = 0;
+            int v = startArray.Length;
+            for (int i = 0; i < v; i++)
+                if (startArray[i].Length > n)
+                {
+                    n = startArray[i].Length;
+                    m = i;
+                }
+                    Console.WriteLine("{0}", startArray[m]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание 6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку без знаков препинания");
+            string startString = Console.ReadLine();
+            int nt = startString.Length;
+            string startString1 = startString.Substring(0, nt).ToLower();
+            string nospace = startString1.Replace(" ","");
+            int n = nospace.Length;
+            char[] plindrom = nospace.ToCharArray();
+            bool flag = true;
+            int m = (n / 2);
+            for (int i = 0; i < m; i++)
+            {
+                if (plindrom[i] != plindrom[n - i - 1])
+                {
+                    flag = false;
+                }
+            }
+            if (flag)
+            {
+                Console.WriteLine(" Введеная строка являтся полиндромом!");
+            }
+            else
+            {
+                Console.WriteLine(" Введеная строка не являтся полиндромом!");
+            }
+                Console.ReadKey();                     
+        }
+    }
+}
+ 
